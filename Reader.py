7a4bdc4e6cd392f62e17bfcbb831cc6bdcc45835
Patrick <xa.patjb4@gmail.com>
2019-08-17 13:52:05 -0400

    var now incremented when int float array or char is found

--- Reader.py
+++ Reader.py
@@ -47,6 +47,7 @@ def word_line(file,words):
 #Returns a dictionnary containing the number of instances of words found
 def word_count(file, words):
     #make a dict with words we're looking for
+    var_tags = ['in>', 'fl>', 'ar>', 'ch>']
     counts = dict.fromkeys(words,0)
     with open(file) as f:
         for i,x in enumerate(f, start=0):
@@ -55,11 +56,16 @@ def word_count(file, words):
             if common:
                 #Increment number of instances for word found
                 for word in common:
+                    #possibly add check to increment var if in fl or ar
                     counts[word] += 1
+                    if word in var_tags:
+                        counts['va>'] += 1
+
     return counts
 
 #Take in a list of variable names and return the average length
 def list_avg(variables):
+    print(variables)
     if variables:
         return sum(len(word) for word in variables) / len(variables)
     return 0
@@ -67,15 +73,17 @@ def list_avg(variables):
 #makes a list of whatever is tagged in va to return avg length
     #find va> then add var name to list
 def var_avg(file):
+    var_tags = ['in>', 'fl>', 'ar>', 'ch>', 'va>']
     var_names = []
     with open(file) as f:
         for i,x in enumerate(f, start=0):
-            common = set(word_find(x,'va>'))
+            common = set(word_find(x,var_tags))
             if common:
+                print('line', i)
                 line = x.strip().split()
                 #for every index containing va> add the following index to the list
                 for x in range(0,len(line)):
-                    if line[x] == 'va>':
+                    if line[x] in var_tags:
                         var_names.append(line[x+1])
                         x+=1
     return list_avg(var_names)
